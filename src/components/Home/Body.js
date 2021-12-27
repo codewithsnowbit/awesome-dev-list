7bfc275b44cd5a03025f74cbe18c27781b0cb884
SnowBit <dtshah24022006@gmail.com>
2021-12-27 18:56:06 +0530

    Fix logo

--- src/components/Home/Body.js
+++ src/components/Home/Body.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import '../../styles/Home.css'
+import '../../styles/Nav.css'
 import Lists from './Lists'
 
 
 function Body() {
     return (
         <div>
-            <h1 className="logo header-logo animate__animated animate__bounce"><span>Awesome-Dev-List</span></h1>
+            <h1 className="logo header-logo"><span>Awesome-Dev-List</span></h1>
             <div className="box">
                 <h2 className="mb-4"><b>Resources</b></h2>                
                 <Lists />
