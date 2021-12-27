131510ba3ad58316ed4098659e5551a4275b0aa4
SnowBit <dtshah24022006@gmail.com>
2021-12-27 18:57:22 +0530

    Add animation

--- src/components/Home/Body.js
+++ src/components/Home/Body.js
@@ -7,7 +7,7 @@ import Lists from './Lists'
 function Body() {
     return (
         <div>
-            <h1 className="logo header-logo"><span>Awesome-Dev-List</span></h1>
+            <h1 className="logo header-logo animate__animated animate__backInDown"><span>Awesome-Dev-List</span></h1>
             <div className="box">
                 <h2 className="mb-4"><b>Resources</b></h2>                
                 <Lists />
