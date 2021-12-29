a08ddcba62269eb45e9f2882d70dd35efa0bc3bd
SnowBit <dtshah24022006@gmail.com>
2021-12-29 16:37:15 +0530

    Add down arrow to the page

--- src/components/Home/Body.js
+++ src/components/Home/Body.js
@@ -7,7 +7,14 @@ import Lists from './Lists'
 function Body() {
     return (
         <div>
-            <h1 className="logo header-logo animate__animated animate__backInDown"><span>Awesome-Dev-List</span></h1>
+            <div className="header-logo ">
+                <div>
+                <h1 className="logo animate__animated animate__backInDown"><span>Awesome-Dev-List</span></h1>
+                <p className="text-center mt-5">
+                    <img src="assets/down-arrow.jpg" alt="Down arrow" className="img-fluid arrow animate__animated animate__backInUp"/>
+                </p>
+                </div>
+            </div>
             <div className="box">
                 <h2 className="mb-4"><b>Top Stories</b></h2>                
                 <Lists />
